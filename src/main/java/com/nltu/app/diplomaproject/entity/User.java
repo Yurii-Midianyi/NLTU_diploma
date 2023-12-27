627f93de3c4ca9aa903dd283a7793de0a0aea838
Yurii Midianyi <loopfordev@gmail.com>
2023-12-27 13:55:53 +0200

    added create table user changelog

--- src/main/java/com/nltu/app/diplomaproject/entity/User.java
+++ src/main/java/com/nltu/app/diplomaproject/entity/User.java
@@ -1,5 +1,6 @@
 package com.nltu.app.diplomaproject.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,8 +12,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
     public Long getId() {
