c548330d59b66e0a41ec697b58e2609a11df1900
Yurii Midianyi <loopfordev@gmail.com>
2024-01-07 22:56:37 +0200

    added ids to answerDtos

--- src/main/java/com/nltu/app/diplomaproject/dto/AnswerDto.java
+++ src/main/java/com/nltu/app/diplomaproject/dto/AnswerDto.java
@@ -1,12 +1,22 @@
 package com.nltu.app.diplomaproject.dto;
 
 public class AnswerDto {
+    private Long id;
     private String answerText;
 
     public AnswerDto() {}
 
-    public AnswerDto(String answerText) {
+    public AnswerDto(Long id, String answerText) {
+        this.id = id;
         this.answerText = answerText;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getAnswerText() {
